923d99eaf3e48441e5dbfe09bbb853fbf721e29e
DIOURI Yassine <yassinediouri92@gmail.com>
2025-09-07 23:09:41 +0200

    [T2] Create Spring Boot project and Prepare Dockerfile/docker compose

---
+++ back/src/main/java/yas/dio/product_trial/ProductTrialApplication.java
@@ -0,0 +1,11 @@
+package yas.dio.product_trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductTrialApplication.class, args);
+    }
+}
